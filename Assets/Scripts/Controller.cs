2c104c0ab3bb234f5a7de957545c0c9b94595bd6
Simone Di Giorgi <simonetomi@gmail.com>
2018-02-06 14:59:32 +0100

    Controller script, Simone scene

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    private float speed = 200;
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        if (Input.GetMouseButton(1))
+        {
+            transform.GetChild(1).Rotate(0, 0, speed * Time.deltaTime);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            transform.GetChild(2).Rotate(0, 0, -speed * Time.deltaTime);
+        }
+
+    }
+}
