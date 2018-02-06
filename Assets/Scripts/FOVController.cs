e61d206c58644bb95a42a2de6ead0e98deab0ab4
Simone Di Giorgi <simonetomi@gmail.com>
2018-02-06 16:39:47 +0100

    FOVController, Dotween

---
+++ Assets/Scripts/FOVController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOVController : MonoBehaviour {
+
+    public Camera maincamera;
+    public float FOV, speed;
+
+	void Update () {
+        maincamera.fieldOfView = Mathf.MoveTowards(maincamera.fieldOfView, FOV, Time.deltaTime * speed);
+    }
+
+}
