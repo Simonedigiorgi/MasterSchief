1802e040b29950b159cabcd8b33001b29d27b876
Escher <dbga_40@WKST_40.dbga.local>
2018-02-06 20:01:12 +0100

    sto pushando gabri

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,30 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-    public float maxBarLength = 10;
-    public float barChangeAmount;
-    Scrollbar bar;
+    public GameObject[] buttonPunch; //List of buttons
+    int buttonChosen; //this button will be chosen random and activate to let our player punch
+    public int countDown;
 
-	// Use this for initialization
-	void Start () {
-        bar = GameObject.Find("Scrollbar").GetComponent<Scrollbar>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    public void ChangeValue(SpawnColor type)
+    public float secondsBeforeStart = 3;
+    public float minDelay;
+    public float maxDelay;
+    [Range(0, 1)]
+    public float buttonEventChance;
+    public IEnumerator SecondsBeforeStart()
     {
-        if (type == SpawnColor.RED)
-            bar.value -= barChangeAmount / maxBarLength;
-
-        else if (type == SpawnColor.BLUE)
-            bar.value += barChangeAmount / maxBarLength;
+        yield return new WaitForSeconds(secondsBeforeStart);
+        YouPunch();
     }
 
+    public IEnumerator YouPunch()
+    {
+        countDown = 3;
+        buttonChosen = Random.Range(0, buttonPunch.Length);
+        buttonPunch[buttonChosen].SetActive(true);
+        yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
+        if (Random.value <= buttonEventChance)
+            StartCoroutine(YouPunch());
+        else StartCoroutine(YouParry());
+    }
+
+    public IEnumerator YouParry()
+    {
+        return null;
+    }
 
 }
