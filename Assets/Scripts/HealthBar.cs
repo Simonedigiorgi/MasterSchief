5bcfd833229a1811bf15303db9ff33822002d8aa
Simone Di Giorgi <simonetomi@gmail.com>
2018-02-06 20:09:36 +0100

    HealthBar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    public Image heroHealth;
+    public Image enemyHealth;
+
+    private float hitpoint = 100;
+    private float maxHitpoint = 100;
+
+    private float enemyhitpoint = 100;
+    private float enemymaxHitpoint = 100;
+
+    public float damage = 20;
+
+	void Start () {
+        UpdateHealthBar();
+        EnemyHealthBar();
+
+    }
+	
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            TakeDamage();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            EnemyDamage();
+        }
+    }
+
+    private void UpdateHealthBar()
+    {
+        float ratio = hitpoint / maxHitpoint;
+        heroHealth.rectTransform.localScale = new Vector3(ratio, 1, 1);
+    }
+
+    private void EnemyHealthBar()
+    {
+        float enemyratio = enemyhitpoint / enemymaxHitpoint;
+        enemyHealth.rectTransform.localScale = new Vector3(enemyratio, 1, 1);
+    }
+
+    private void TakeDamage()
+    {
+        hitpoint -= damage;
+
+        if(hitpoint <= 0)
+        {
+            hitpoint = 0;
+            Debug.Log("YOU LOSE");
+        }
+
+        UpdateHealthBar();
+    }
+
+    private void EnemyDamage()
+    {
+        enemyhitpoint -= damage;
+
+        if (enemyhitpoint <= 0)
+        {
+            enemyhitpoint = 0;
+            Debug.Log("YOU WON");
+        }
+
+        EnemyHealthBar();
+    }
+}
