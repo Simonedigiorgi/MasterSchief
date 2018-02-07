94f10b45cb446251eb37a66159d43e67c9558f21
Gabriele Dolcemascolo <dolce.9951@gmail.com>
2018-02-07 19:00:28 +0100

    yee

--- Assets/Scripts/ButtonCountdown.cs
+++ Assets/Scripts/ButtonCountdown.cs
@@ -27,8 +27,8 @@ public class ButtonCountdown : MonoBehaviour {
         if(timer>countDownTime)
         {
             this.gameObject.SetActive(false);
-            enemyAnimator.SetTrigger("punch");
-            hb.TakeDamage();
+            //enemyAnimator.SetTrigger("punch");
+            //hb.TakeDamage();
         }
 	}
 }
