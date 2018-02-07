6d02a72f960a0470c5d39d165cedc1d667302e53
Gabriele Dolcemascolo <dolce.9951@gmail.com>
2018-02-07 19:44:34 +0100

    bastianich infame per te solo lame

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -29,6 +29,8 @@ public class PlayerActions : MonoBehaviour {
     public Image blackFade;
 
 
+
+
     public float endGameTime = 30;
     float endTimer = 0;
     public int endGameClickAmount = 40;
@@ -38,6 +40,16 @@ public class PlayerActions : MonoBehaviour {
     bool levelFailed = false;
 
     public string nextScene = "Cannavacciuolo";
+
+
+    public GameObject[] enemiesPunch;
+    public GameObject[] playerPunch;
+    public GameObject parat;
+
+    public Transform destra;
+    public Transform sinistra;
+    public Transform p;
+
 	// Use this for initialization
 	void Start () {
         hb = GameObject.Find("Background").GetComponent<HealthBar>();
@@ -47,6 +59,35 @@ public class PlayerActions : MonoBehaviour {
 
     bool toggle = false;
 
+
+    public void SpawnPunchInfame()
+    {
+        Instantiate(enemiesPunch[0], p);
+    }
+
+    public void SpawnChargeInfame()
+    {
+        Instantiate(enemiesPunch[1], p);
+    }
+
+    public void SpawnPlayerInfame()
+    {
+        if(Random.value > 0.5f)
+        {
+            GameObject s = Instantiate(playerPunch[Random.Range(0, playerPunch.Length)], destra);
+            s.transform.position += new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), 0);
+        }
+        else
+        {
+            GameObject s = Instantiate(playerPunch[Random.Range(0, playerPunch.Length)], sinistra);
+            s.transform.position += new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), 0);
+        }
+    }
+
+    public void SpawnParat()
+    {
+        GameObject s = Instantiate(parat, p);
+    }
 
     // Update is called once per frame
     void Update () {
@@ -70,11 +111,13 @@ public class PlayerActions : MonoBehaviour {
                         {
                             toggle = !toggle;
                             leftArmAnimator.SetTrigger("punch");
+                            SpawnPlayerInfame();
                         }
                         else
                         {
                             toggle = !toggle;
                             rightArmAnimator.SetTrigger("punch");
+                            SpawnPlayerInfame();
                         }
 
                         enemyAnimator.SetTrigger("takeDamage");
@@ -102,6 +145,7 @@ public class PlayerActions : MonoBehaviour {
                         leftArmAnimator.SetTrigger("punch");
                         hit.collider.gameObject.SetActive(false);
                         hb.EnemyDamage("left");
+                        SpawnPlayerInfame();
 
                     }
                     else if (hit.collider.tag == "ButtonRight")
@@ -109,6 +153,7 @@ public class PlayerActions : MonoBehaviour {
                         rightArmAnimator.SetTrigger("punch");
                         hit.collider.gameObject.SetActive(false);
                         hb.EnemyDamage("right");
+                        SpawnPlayerInfame();
                     }
                     else
                     {
