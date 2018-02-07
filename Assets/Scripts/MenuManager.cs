525be361e0beb429c882a0f15948ff4692312a00
Simone Di Giorgi <simonetomi@gmail.com>
2018-02-07 13:36:55 +0100

    Menu senza grafica

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,16 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using DG.Tweening;
 
 public class MenuManager : MonoBehaviour {
 
-    public Image fade;
-    public Text introText;
+    public Transform startgamePivot;
 
-	// Use this for initialization
-	void Start () {
-        StartCoroutine(Intro());
+    private AudioSource source;
+    private AudioClip smash;
+
+    public Button newGame;
+    public Button controls;
+    public Button quit;
+
+    public Image newGameClick;
+    public Image controlClick;
+    public Image quitClick;
+
+    public Image fade;
+
+    // Use this for initialization
+    void Start () {
+        fade.enabled = false;
+        fade.DOFade(0, 0);
 	}
 	
 	// Update is called once per frame
@@ -21,7 +35,7 @@ public class MenuManager : MonoBehaviour {
 
 	}
 
-    public IEnumerator Intro()
+    /*public IEnumerator Intro()
     {
         introText.text = "2018";
         //ntroText.DOFade(1, 1);
@@ -43,5 +57,75 @@ public class MenuManager : MonoBehaviour {
         introText.DOFade(1, 1);
         yield return new WaitForSeconds(2);
         introText.DOFade(0, 1);
+    }*/
+
+    public void StartGame()
+    {
+        StartCoroutine(StartGameRoutine());
+    }
+
+    public void Controls()
+    {
+        StartCoroutine(ControlsRoutine());
+    }
+
+    public void Quit()
+    {
+        StartCoroutine(QuitRoutine());
+    }
+
+    public IEnumerator StartGameRoutine()
+    {
+        fade.enabled = true;
+        newGameClick.enabled = true;
+        newGame.enabled = false;
+        controls.enabled = false;
+        quit.enabled = false;
+        yield return new WaitForSeconds(2.0f);
+        fade.DOFade(1, 2);
+        yield return new WaitForSeconds(2.0f);
+        //SceneManager.LoadScene();
+    }
+
+    public IEnumerator ControlsRoutine()
+    {
+        /*//controlClick.enabled = true;
+        newGame.enabled = false;
+        controls.enabled = false;
+        quit.enabled = false;
+        //yield return new WaitForSeconds(2.0f);
+        startgamePivot.DOMoveX(-300, 0.8f);
+        yield return new WaitForSeconds(2.0f);
+        //SceneManager.LoadScene();*/
+        fade.enabled = true;
+        controlClick.enabled = true;
+        newGame.enabled = false;
+        controls.enabled = false;
+        quit.enabled = false;
+        yield return new WaitForSeconds(2.0f);
+        fade.DOFade(1, 2);
+        yield return new WaitForSeconds(2.0f);
+        //SceneManager.LoadScene();
+    }
+
+    public IEnumerator QuitRoutine()
+    {
+        /*//controlClick.enabled = true;
+        newGame.enabled = false;
+        controls.enabled = false;
+        quit.enabled = false;
+        //yield return new WaitForSeconds(2.0f);
+        startgamePivot.DOMoveX(-300, 0.8f);
+        yield return new WaitForSeconds(2.0f);
+        //SceneManager.LoadScene();*/
+        fade.enabled = true;
+        quitClick.enabled = true;
+        newGame.enabled = false;
+        controls.enabled = false;
+        quit.enabled = false;
+        yield return new WaitForSeconds(2.0f);
+        fade.DOFade(1, 2);
+        yield return new WaitForSeconds(2.0f);
+        //SceneManager.LoadScene();
     }
 }
