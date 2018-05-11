e88a0302ba0a26eb9e7f9db4ce06e8d315b6ece3
Simone Di Giorgi <simonetomi@gmail.com>
2018-05-12 00:53:12 +0200

    Loading screen

---
+++ Assets/Scripts/LoadingManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingManager : MonoBehaviour {
+
+    public Image[] loadingImages;
+    public Text text;
+    public int i = 0;
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+        loadingImages[i].enabled = true;
+
+        if (Input.GetButtonUp("Button Down/Left"))
+        {
+            i++;
+        }
+
+        if(i == 0)
+        {
+            text.text = "Quando vedi queste icone premi il tasto corrispondente del tuo controller per attaccare lo chef";
+        }
+
+        if(i == 1)
+        {
+            text.text = "Messaggio 2";
+        }
+
+        if (i == 2)
+        {
+            text.text = "Messaggio 3";
+        }
+
+        if(i == 3)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+    }
+}
