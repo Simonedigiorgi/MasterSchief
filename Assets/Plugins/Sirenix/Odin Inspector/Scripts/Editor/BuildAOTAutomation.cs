17f55b2db7907bbb4db7b108deff89c23b38cf96
DaaMonkey <simonetomi@gmail.com>
2019-05-30 19:24:34 +0200

    Riduzione app

--- Assets/Plugins/Sirenix/Odin Inspector/Scripts/Editor/BuildAOTAutomation.cs
+++ Assets/Plugins/Sirenix/Odin Inspector/Scripts/Editor/BuildAOTAutomation.cs
@@ -1,4 +1,11 @@
-﻿#if UNITY_5_6_OR_NEWER
+﻿#if UNITY_EDITOR
+#if UNITY_5_6_OR_NEWER
+
+//-----------------------------------------------------------------------
+// <copyright file="BuildAOTAutomation.cs" company="Sirenix IVS">
+// Copyright (c) Sirenix IVS. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace Sirenix.Serialization.Internal
 {
@@ -6,52 +13,69 @@ namespace Sirenix.Serialization.Internal
     using UnityEditor;
     using UnityEditor.Build;
     using System.IO;
+    using System;
 
+#if UNITY_2018_1_OR_NEWER
+
+    using UnityEditor.Build.Reporting;
+
+#endif
+
+#if UNITY_2018_1_OR_NEWER
+    public class PreBuildAOTAutomation : IPreprocessBuildWithReport
+#else
     public class PreBuildAOTAutomation : IPreprocessBuild
+#endif
     {
-        public int callbackOrder
-        {
-            get
-            {
-                return -1000;
-            }
-        }
+        public int callbackOrder { get { return -1000; } }
 
         public void OnPreprocessBuild(BuildTarget target, string path)
         {
-            if (AOTGenerationConfig.Instance.AutomateBeforeBuilds
-                && AOTGenerationConfig.Instance.AutomateForPlatforms != null
-                && AOTGenerationConfig.Instance.AutomateForPlatforms.Contains(target))
+            if (AOTGenerationConfig.Instance.ShouldAutomationGeneration(target))
             {
                 AOTGenerationConfig.Instance.ScanProject();
                 AOTGenerationConfig.Instance.GenerateDLL();
             }
         }
+
+#if UNITY_2018_1_OR_NEWER
+
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            this.OnPreprocessBuild(report.summary.platform, report.summary.outputPath);
+        }
+
+#endif
     }
 
+#if UNITY_2018_1_OR_NEWER
+    public class PostBuildAOTAutomation : IPostprocessBuildWithReport
+#else
     public class PostBuildAOTAutomation : IPostprocessBuild
+#endif
     {
-        public int callbackOrder
-        {
-            get
-            {
-                return -1000;
-            }
-        }
+        public int callbackOrder { get { return -1000; } }
 
         public void OnPostprocessBuild(BuildTarget target, string path)
         {
-            if (AOTGenerationConfig.Instance.AutomateBeforeBuilds
-                && AOTGenerationConfig.Instance.AutomateForPlatforms != null
-                && AOTGenerationConfig.Instance.AutomateForPlatforms.Contains(target)
-                && AOTGenerationConfig.Instance.DeleteDllAfterBuilds)
+            if (AOTGenerationConfig.Instance.DeleteDllAfterBuilds && AOTGenerationConfig.Instance.ShouldAutomationGeneration(target))
             {
                 Directory.Delete(AOTGenerationConfig.Instance.AOTFolderPath, true);
                 File.Delete(AOTGenerationConfig.Instance.AOTFolderPath.TrimEnd('/', '\\') + ".meta");
                 AssetDatabase.Refresh();
             }
         }
+
+#if UNITY_2018_1_OR_NEWER
+
+        public void OnPostprocessBuild(BuildReport report)
+        {
+            this.OnPostprocessBuild(report.summary.platform, report.summary.outputPath);
+        }
+
+#endif
     }
 }
 
+#endif
 #endif
